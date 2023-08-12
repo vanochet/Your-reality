8ab07c377c5b8964282952e3ef60e54dcca6ab2a
vanochet <talismanchet@vk.com>
2023-08-12 16:35:32 +0300

    Initial commit

---
+++ scripts/Player.gd
@@ -0,0 +1,85 @@
+extends KinematicBody
+
+export var speed: float = 10.0
+export var weapon: int = 0.0
+export var gravity: float = -24.8
+export var accel: float = 4.5
+export var deaccel: float = 16.0
+export var MAX_SLOPE_ANGLE: float = 89.0
+export var mouse_sensitivity: float = 0.05
+export var jump_strength: float = 9.0
+
+onready var camera = $Head/Camera
+onready var head = $Head
+
+var dir
+var vel: Vector3 = Vector3()
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _process(delta):
+	dir = Vector3()
+	var cam_xform = camera.get_global_transform()
+	var input_movement_vector = Vector2()
+	if Input.is_action_pressed("ui_up"):
+		input_movement_vector.y += 1
+	if Input.is_action_pressed("ui_down"):
+		input_movement_vector.y -= 1
+	if Input.is_action_pressed("ui_left"):
+		input_movement_vector.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		input_movement_vector.x += 1
+	
+	if Input.is_action_just_pressed("ui_focus_next"):
+		OS.window_fullscreen = !OS.window_fullscreen
+	
+	input_movement_vector = input_movement_vector.normalized()
+	
+	dir += -cam_xform.basis.z * input_movement_vector.y
+	dir += cam_xform.basis.x * input_movement_vector.x
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			vel.y = jump_strength
+	
+	if Input.is_action_just_pressed("ui_alt"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	
+	dir.y = 0
+	dir = dir.normalized()
+	
+	vel.y += delta * gravity
+	
+	var hvel = vel
+	hvel.y = 0
+	
+	var target = dir
+	target *= speed
+	
+	var do_accel
+	if dir.dot(hvel) > 0:
+		do_accel = accel
+	else:
+		do_accel = deaccel
+	
+	hvel = hvel.linear_interpolate(target, do_accel * delta)
+	vel.x = hvel.x
+	vel.z = hvel.z
+	vel = move_and_slide(vel, Vector3(0, 1, 0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE))
+
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		head.rotate_x(deg2rad((-event.relative.y) * mouse_sensitivity))
+		self.rotate_y(deg2rad(event.relative.x * mouse_sensitivity * -1))
+		
+		var camera_rot = head.rotation_degrees
+		camera_rot.x = clamp(camera_rot.x, -70, 70)
+		head.rotation_degrees = camera_rot
---
+++ scripts/Table.gd
@@ -0,0 +1,16 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	look_at(get_parent().get_node("Player").transform.origin, Vector3(0.0, 1.0, 0.0))
